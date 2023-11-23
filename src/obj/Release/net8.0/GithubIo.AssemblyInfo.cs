55e21aa2d48bdb31a5e1b3e5be7e055c5bf23917
Róbert Fuszenecker <rfuszenecker@diligent.com>
2023-11-23 17:19:34 +0100

    Pub.

--- src/obj/Release/net8.0/GithubIo.AssemblyInfo.cs
+++ src/obj/Release/net8.0/GithubIo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GithubIo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a75bb3cc29f94cf296bac1220e9c51ddc0fea3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+919c8579d4080daa469ea7b5e25a702e24b5868f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GithubIo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GithubIo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
